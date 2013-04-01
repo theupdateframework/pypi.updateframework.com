37e9a138902d1c5592fa49951a38e3cc39c6080a
dachshund <trishankkarthik@gmail.com>
2013-04-01 16:41:36 -0400

    Use bandersnatch to mirror PyPI.

--- setup2.sh
+++ setup2.sh
@@ -5,19 +5,27 @@
 source environment.sh
 
 
-cd $BASE_DIRECTORY
-
-
 # Mirror PyPI.
-if [ ! -d $VIRTUAL_ENVIRONMENT ]
+if [ ! -d $BASE_DIRECTORY/$VIRTUAL_ENVIRONMENT ]
 then
   echo "Please run setup1.sh first!"; exit 1;
 else
+
+  cp bandersnatch.conf $BASE_DIRECTORY
+  cd $BASE_DIRECTORY
   source $VIRTUAL_ENVIRONMENT/bin/activate
-  # TODO: Consider https://pypi.python.org/pypi/bandersnatch
-  pip install --upgrade pep381client
-  mkdir -p $PYPI_MIRROR_DIRECTORY
-  cd $PYPI_MIRROR_DIRECTORY
-  # If you run into trouble, goto https://bitbucket.org/loewis/pep381client/
-  pep381run .
+
+  # Do we not have bandersnatch?
+  if [ ! -d bandersnatch ]
+  then
+    # Install and setup bandersnatch.
+    sudo apt-get install mercurial
+    hg clone https://bitbucket.org/ctheune/bandersnatch
+    cd bandersnatch
+    python bootstrap.py
+    bin/buildout
+    cd $BASE_DIRECTORY
+  fi
+
+  bandersnatch/bin/bsn-mirror -c bandersnatch.conf
 fi
