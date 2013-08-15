9ea599d0bb5b8ebfc8cc90f57089facf43fd99ca
dachshund <trishankkarthik@gmail.com>
2013-08-14 21:11:28 -0400

    Fix bug.

--- setup4.sh
+++ setup4.sh
@@ -6,7 +6,7 @@ source environment.sh
 
 
 # Activate virtual environment.
-if [ ! -d $BASE_DIRECTORY/$VIRTUALENV_PACKAGE ]
+if [ ! -d $BASE_DIRECTORY/$VIRTUAL_ENVIRONMENT ]
 then
   echo "Please run setup1.sh first!"; exit 1;
 else
